f6116061fa56ca42a6bf82a6949c4197f4fb0e55
Luca Succi <luca.succi@stritzinger.com>
2026-01-26 17:26:47 +0100

    Cleanup and adapt partial build flow

--- src/cassone.erl
+++ src/cassone.erl
@@ -2,6 +2,11 @@
 
 -export([init/1]).
 
+-export_types([os/0, arch/0]).
+
+-type os() :: linux | macos.
+-type arch() :: x86_64 | aarch64.
+
 -spec init(rebar_state:t()) -> {ok, rebar_state:t()}.
 init(State) ->
     {ok, State1} = cassone_prv:init(State),
--- src/cassone_prv.erl
+++ src/cassone_prv.erl
@@ -25,21 +25,20 @@ init(State) ->
 
 -spec do(rebar_state:t()) -> {ok, rebar_state:t()} | {error, string()}.
 do(State) ->
-    BinDir = filename:join(rebar_dir:base_dir(State), "bin"),
-    WorkingDir = filename:join(rebar_dir:base_dir(State), "cassone"),
-    rebar_utils:sh(io_lib:format("rm -rf ~s", [WorkingDir]), []),
-    ok = filelib:ensure_path(BinDir),
-    ok = filelib:ensure_path(WorkingDir),
+    EscriptDir = filename:join(rebar_dir:base_dir(State), "bin"),
+    CassoneDir = filename:join(rebar_dir:base_dir(State), "cassone"),
+    rebar_utils:sh(io_lib:format("rm -rf ~s", [CassoneDir]), []),
+    ok = filelib:ensure_path(EscriptDir),
+    ok = filelib:ensure_path(CassoneDir),
     Config = rebar_state:get(State, cassone, []),
     Mode = get_option(mode, Config, release),
     Targets = get_option(targets, Config, [current_machine]),
     Opts = #{
         mode => Mode,
-        bin_dir => BinDir,
-        working_dir => WorkingDir
+        escript_dir => EscriptDir,
+        cassone_dir => CassoneDir
     },
     {Opts2, State1} = release(Mode, Opts, State),
-    prepare_piadina(WorkingDir),
     [begin
         cleanup_working_dir(Opts2),
         assemble_target(TGT, Opts2)
@@ -57,7 +56,7 @@ release(escript, Opts, State) ->
     {ok, S} = rebar_prv_escriptize:do(State),
     EscriptName = rebar_state:get(State, escript_name),
     {Opts#{escript_name => atom_to_list(EscriptName)}, S};
-release(release, Opts, State) ->
+release(release, _, _) ->
     rebar_api:abort("cassone: release mode is not supported yet", []).
 
 get_option(Option, Config, Default) ->
@@ -66,54 +65,46 @@ get_option(Option, Config, Default) ->
         false -> Default
     end.
 
-cleanup_working_dir(#{working_dir := WorkingDir, escript_name := EscriptName}) ->
+cleanup_working_dir(#{cassone_dir := CassoneDir, escript_name := EscriptName}) ->
     rebar_api:info("cassone: cleaning up working directory", []),
-    ReleaseDir = filename:join([WorkingDir, EscriptName]),
+    ReleaseDir = filename:join([CassoneDir, "working", EscriptName]),
     rebar_utils:sh(io_lib:format("rm -rf ~s", [ReleaseDir]), []),
-    rebar_api:info("cassone: cleanup piadina repository", []),
-    PiadinaDir = filename:join([WorkingDir, "piadina"]),
-    rebar_utils:sh("cd " ++ PiadinaDir ++ " && git clean -fdx", []),
     ok.
 
-assemble_target(current_machine, Options) ->
-    rebar_api:info("cassone: assembling current machine", []),
-    LocalErlangReleaseDir = code:root_dir(),
-    copy_released_files(Options),
-    copy_erts_and_libs(LocalErlangReleaseDir, Options),
-    build_piadina(Options);
 assemble_target({OS, Arch}, Options) ->
     OtpVersion = otp_version(),
-    TargetOTPBuildDir = cassone_erts:fetch(OtpVersion, Arch, OS),
+    TargetOTPDir = cassone_erts:fetch(OtpVersion, OS, Arch),
+    {PiadinaPath, AzdoraPath} = cassone_piadina:fetch(OS, Arch),
     copy_released_files(Options),
-    copy_erts_and_libs(TargetOTPBuildDir, Options),
-    build_piadina(Options);
-assemble_target(Target, Options) ->
+    copy_erts_and_libs(TargetOTPDir, Options),
+    cook(AzdoraPath, PiadinaPath, Options);
+assemble_target(Target, _) ->
     rebar_api:warning("cassone: unsupported target: ~p", [Target]).
 
 copy_released_files(#{
     mode := escript,
-    working_dir := WorkingDir,
-    bin_dir := BinDir,
+    cassone_dir := CassoneDir,
+    escript_dir := EscriptDir,
     escript_name := EscriptName
 }) ->
     rebar_api:info("cassone: copying released files", []),
-    RelEscriptLocation = filename:join([BinDir, EscriptName]),
-    RelEscriptDst = filename:join([WorkingDir, EscriptName, "bin", EscriptName]),
+    RelEscriptLocation = filename:join([EscriptDir, EscriptName]),
+    RelEscriptDst = filename:join([CassoneDir, EscriptName, "bin", EscriptName]),
     filelib:ensure_dir(RelEscriptDst),
     cp_cmd(RelEscriptLocation, RelEscriptDst).
 
-copy_erts_and_libs(TargetOTPInstallation,#{
+copy_erts_and_libs(TargetOTPDir, #{
     mode := escript,
-    working_dir := WorkingDir,
-    bin_dir := BinDir,
+    cassone_dir := CassoneDir,
     escript_name := EscriptName
 }) ->
-    rebar_api:info("cassone: copying erts and libs", []),
-    [ErtsFolder | _] = filelib:wildcard("erts-*", TargetOTPInstallation),
-    ErtsFullPath = filename:join([TargetOTPInstallation, ErtsFolder]),
-    cp_cmd(ErtsFullPath, filename:join([WorkingDir, EscriptName])),
-    LibDir = filename:join([TargetOTPInstallation, "lib"]),
-    cp_cmd(LibDir, filename:join([WorkingDir, EscriptName])),
+    rebar_api:info("cassone: copying erts and libs from ~p to ~p", [TargetOTPDir, CassoneDir]),
+    [ErtsFolder | _] = filelib:wildcard("*/erts-*", TargetOTPDir),
+    ErtsFullPath = filename:join([TargetOTPDir, ErtsFolder]),
+    cp_cmd(ErtsFullPath, filename:join([CassoneDir, EscriptName])),
+    [LibFolder | _] = filelib:wildcard("*/lib", TargetOTPDir),
+    LibDir = filename:join([TargetOTPDir, LibFolder]),
+    cp_cmd(LibDir, filename:join([CassoneDir, EscriptName])),
     ok.
 
 cp_cmd(Src, Dst) ->
@@ -126,32 +117,12 @@ cp_cmd(Src, Dst) ->
     end,
     rebar_utils:sh(io_lib:format("cp ~s ~s ~s", [Flags, Src, Dst]), []).
 
-get_erts_version(LocalErlangReleaseDir) ->
-    StartErlData = filename:join([LocalErlangReleaseDir, "releases", "start_erl.data"]),
-    {ok, Bin} = file:read_file(StartErlData),
-    [ErtVersion, _] = binary:split(Bin, <<" ">>, [global]),
-    ErtVersion.
-
 otp_version() ->
     Root = code:root_dir(),
     Major = erlang:system_info(otp_release),
     {ok, Version} = file:read_file(filename:join([Root, "releases", Major, "OTP_VERSION"])),
     string:trim(binary_to_list(Version)).
 
-prepare_piadina(WorkingDir) ->
-    rebar_api:info("cassone: preparing piadina", []),
-    GitResource = {git, "https://github.com/stritzinger/piadina.git", {branch, "main"}},
-    PiadinaDir = filename:join([WorkingDir, "piadina"]),
-    case filelib:is_dir(PiadinaDir) of
-        false -> rebar_git_resource:download(PiadinaDir, GitResource, []);
-        true -> ok
-    end.
-
-build_piadina(#{working_dir := WorkingDir}) ->
-    rebar_api:info("cassone: building piadina", []),
-    PiadinaDir = filename:join([WorkingDir, "piadina"]),
-    Opts = [{cd, PiadinaDir}],
-    rebar_utils:sh("./autogen.sh", Opts),
-    rebar_utils:sh("./configure ", Opts),
-    rebar_utils:sh("make", Opts),
+cook(AzdoraPath, PiadinaPath, Options) ->
+    rebar_api:info("cassone: cooking", []),
     ok.
